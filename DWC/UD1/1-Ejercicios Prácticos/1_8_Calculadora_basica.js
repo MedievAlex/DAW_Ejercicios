7d9257808cc47607987c1e6b48f858a6757f26e4
Alex <alexirazola@gmail.com>
2026-09-22 09:41:23 +0200

    [DWC]: Ejercicio 8 de los ejercicios practicos terminado.

--- DWC/UD1/1-Ejercicios Prácticos/1_8_Calculadora_basica.js
+++ DWC/UD1/1-Ejercicios Prácticos/1_8_Calculadora_basica.js
@@ -3,6 +3,56 @@ Ejercicio 8. Calculadora básica
 Diseñar interfaz con dos inputs y botones de sumar, restar, multiplicar 
 y dividir. Mostrar resultado en pantalla y validar divisiones por cero.
 
-node "UD1\1-Ejercicios Prácticos\1_8_Calculadora_basica.js"
 */
 
+var numUno;
+var numDos;
+var suma;
+var resta;
+var multiplicacion;
+var division;
+
+function suma() {
+    numUno = document.getElementById('numUno').valueAsNumber;
+    numDos = document.getElementById('numDos').valueAsNumber;
+    suma = numUno + numDos;
+
+    alert(numUno + " + " + numDos + " = " + suma);
+}
+
+function resta() {
+    numUno = document.getElementById('numUno').valueAsNumber;
+    numDos = document.getElementById('numDos').valueAsNumber;
+    resta = numUno - numDos;
+
+    alert(numUno + " - " + numDos + " = " + resta);
+}
+
+function multiplicacion() {
+    numUno = document.getElementById('numUno').valueAsNumber;
+    numDos = document.getElementById('numDos').valueAsNumber;
+    multiplicacion = numUno * numDos;
+
+    alert(numUno + " x " + numDos + " = " + multiplicacion);
+}
+
+function division() {
+    numUno = document.getElementById('numUno').valueAsNumber;
+    numDos = document.getElementById('numDos').valueAsNumber;
+
+    if (comprobarDivision(numUno, numDos)) {
+        division = numUno / numDos;
+
+        alert(numUno + " / " + numDos + " = " + division);
+    } else {
+        alert("[ERROR]: No se puede dividir con 0.");
+    }
+}
+
+function comprobarDivision(numA, numB) {
+    if (numA == 0 || numB == 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
