1b4812600c0cbb949f9391ec3175c8c4ad2c24fe
Alex <alexirazola@gmail.com>
2026-09-23 09:18:33 +0200

    [DWC]: Ejercicio 8 mejorado y con borrar funcional.

--- DWC/UD1/1-Ejercicios Prácticos/1_8_Calculadora_basica.js
+++ DWC/UD1/1-Ejercicios Prácticos/1_8_Calculadora_basica.js
@@ -5,67 +5,70 @@ y dividir. Mostrar resultado en pantalla y validar divisiones por cero.
 
 */
 
-var numUno;
-var numDos;
+const numUno = document.getElementById("numUno");
+const numDos = document.getElementById("numDos");
+const pHistorial = document.getElementById("pHistorial");
+
+const btnSuma = document.getElementById("btnSuma");
+const btnResta = document.getElementById("btnResta");
+const btnMult = document.getElementById("btnMult");
+const btnDiv = document.getElementById("btnDiv");
+const btnBor = document.getElementById("btnBor");
+
+var numeroUno;
+var numeroDos;
 var suma;
 var resta;
 var multiplicacion;
 var division;
 var resultado;
 
-function obtenerNumUno(){
-    return document.getElementById("numUno").valueAsNumber;
-}
+btnSuma.addEventListener("click", function () {
+    numeroUno = numUno.valueAsNumber;
+    numeroDos = numDos.valueAsNumber;
+    suma = numeroUno + numeroDos;
 
-function obtenerNumDos(){
-    return document.getElementById("numDos").valueAsNumber;
-}
-
-function obtenerHistorial(){
-    return document.getElementById("pHistorial");
-}
-
-function suma() {
-    numUno = obtenerNumUno();
-    numDos = obtenerNumDos();
-    suma = numUno + numDos;
-
-    resultado = numUno + " + " + numDos + " = " + suma + "</br>";
+    resultado = numeroUno + " + " + numeroDos + " = " + suma + "</br>";
     mostrarResultado(resultado);
-}
+});
 
-function resta() {
-    numUno = obtenerNumUno();
-    numDos = obtenerNumDos();
-    resta = numUno - numDos;
+btnResta.addEventListener("click", function () {
+    numeroUno = numUno.valueAsNumber;
+    numeroDos = numDos.valueAsNumber;
+    resta = numeroUno - numeroDos;
 
-    resultado = numUno + " - " + numDos + " = " + resta + "</br>";
+    resultado = numeroUno + " - " + numeroDos + " = " + resta + "</br>";
     mostrarResultado(resultado);
-}
+});
 
-function multiplicacion() {
-    numUno = obtenerNumUno();
-    numDos = obtenerNumDos();
-    multiplicacion = numUno * numDos;
+btnMult.addEventListener("click", function () {
+    numeroUno = numUno.valueAsNumber;
+    numeroDos = numDos.valueAsNumber;
+    multiplicacion = numeroUno * numeroDos;
 
-    resultado = numUno + " x " + numDos + " = " + multiplicacion + "</br>";
+    resultado = numeroUno + " x " + numeroDos + " = " + multiplicacion + "</br>";
     mostrarResultado(resultado);
-}
+});
 
-function division() {
-    numUno = obtenerNumUno();
-    numDos = obtenerNumDos();
+btnDiv.addEventListener("click", function () {
+    numeroUno = numUno.valueAsNumber;
+    numeroDos = numDos.valueAsNumber;
 
-    if (comprobarDivision(numUno, numDos)) {
-        division = numUno / numDos;
+    if (comprobarDivision(numeroUno, numeroDos)) {
+        division = numeroUno / numeroDos;
 
-        resultado = numUno + " / " + numDos + " = " + division + "</br>";
+        resultado = numeroUno + " / " + numeroDos + " = " + division + "</br>";
     } else {
         resultado = "[ERROR]: No se puede dividir con 0." + "</br>";
     }
 
     mostrarResultado(resultado);
-}
+});
+
+btnBor.addEventListener("click", function () {
+    numUno.value = "";
+    numDos.value = "";
+});
 
 function comprobarDivision(numA, numB) {
     if (numA == 0 || numB == 0) {
@@ -77,5 +80,5 @@ function comprobarDivision(numA, numB) {
 
 function mostrarResultado(res) {
     //alert(res);
-    obtenerHistorial().insertAdjacentHTML("afterbegin", res);
+    pHistorial.insertAdjacentHTML("afterbegin", res);
 }
