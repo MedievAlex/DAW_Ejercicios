0b6d95ef0bd714c74f5fe0049aa998ec3258e4b9
Alex <alexirazola@gmail.com>
2026-09-25 13:29:19 +0200

    [DWS]: ejercicio 4 de la actividad 5 completado.

--- DWS/UD1/Actividad 5/ejercicio04.php
+++ DWS/UD1/Actividad 5/ejercicio04.php
@@ -8,10 +8,35 @@ Build-in: http://localhost:8000/Actividad_5/ejercicio04.php
 Xdebug: http://php-mvc.localhost/Actividad_5/ejercicio04.php
 */
 
+$valores = array(
+    "A" => rand(1, 10),
+    "E" => rand(1, 10),
+    "C" => rand(1, 10),
+    "B" => rand(1, 10),
+    "G" => rand(1, 10),
+    "D" => rand(1, 10),
+    "F" => rand(1, 10)
+);
+
 echo ("Ejercicio 04");
 echo ("</br>-------------------------------</br>");
 
+echo ("ORDEN POR KEY");
 
+ksort($valores);
+
+foreach ($valores as $letra => $numero) {
+    echo ("</br>" . $letra . " - " . $numero);
+}
+echo ("</br>");
+
+echo ("ORDEN NATURAL");
+
+natsort($valores);
+
+foreach ($valores as $letra => $numero) {
+    echo ("</br>" . $letra . " - " . $numero);
+}
 
 echo ("</br>-------------------------------");
 
